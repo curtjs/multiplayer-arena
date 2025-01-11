fb8035fa2bff5aecc51ad165e35c5cdb2ec2d5af
Jonathan Curtis <mrjcurtisdev@gmail.com>
2025-01-11 11:07:44 -0500

    add basic p2p multiplaye

--- player/player.gd
+++ player/player.gd
@@ -3,7 +3,17 @@ extends CharacterBody2D
 const SPEED = 100.0
 const JUMP_VELOCITY = -200.0
 
+func _enter_tree():
+	set_multiplayer_authority(int(str(name)))
+
+func _ready():
+	if !is_multiplayer_authority():
+		$Sprite2D.modulate = Color.RED
+
 func _physics_process(delta):
+	if !is_multiplayer_authority():
+		return
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
